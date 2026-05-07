f08a47bc6c00078a571a91027b45bd37090768a7
Tartarga <109563717+Tartarga@users.noreply.github.com>
2026-05-07 12:45:48 -0500

    Restored Code as requested by Taurenkey

--- WrathCombo/AutoRotation/AutoRotationController.cs
+++ WrathCombo/AutoRotation/AutoRotationController.cs
@@ -58,15 +58,13 @@ internal unsafe class AutoRotationController
     public AutoRotationController()
     {
         OnPartyCombatChanged += ResetError;
-        //Svc.Chat.ChatMessage += ScanForWarnings;
+        Svc.Chat.ChatMessage += ScanForWarnings;
         OnStatusChanged += StatusChanged;
     }
 
     private void StatusChanged(uint statusId, bool onPlayer)
     {
         Svc.Log.Verbose($"[AutoRotStatusCheck] {((ushort)statusId).StatusName()} {(onPlayer ? "Gained" : "Lost")}");
-        //if (statusId == 5191 && !onPlayer)
-        //    Paused = false;
     }
 
     private void ScanForWarnings(Dalamud.Game.Chat.IHandleableChatMessage message)
@@ -79,14 +77,6 @@ internal unsafe class AutoRotationController
         var logMessages = Svc.Data.Excel.GetSheet<LogMessage>();
         switch (Content.TerritoryID)
         {
-            case 1345:
-                if (message.Message.TextValue == logMessages.GetRow(11531).Text)
-                {
-                    pauseWarningFound = true;
-                    UnpauseSeconds = 20;
-                    Svc.Targets.Target = null; //Stop auto-attacks
-                }
-            break;
             default:
                 break;
         }
@@ -101,7 +91,7 @@ internal unsafe class AutoRotationController
     public void Dispose()
     {
         OnPartyCombatChanged -= ResetError;
-        //Svc.Chat.ChatMessage -= ScanForWarnings;
+        Svc.Chat.ChatMessage -= ScanForWarnings;
     }
 
     private void ResetError(bool state)
