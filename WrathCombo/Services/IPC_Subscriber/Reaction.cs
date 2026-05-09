9fc41f935495286565e89e05f8382cffd0a55a57
Ethan Henderson <ethan@zbee.codes>
2026-05-09 15:12:25 -0600

    feat(Conflicts): Remove Reaction targeting conflict

--- WrathCombo/Services/IPC_Subscriber/Reaction.cs
+++ WrathCombo/Services/IPC_Subscriber/Reaction.cs
@@ -96,21 +96,6 @@ public class ReActionIPC(
         }
     }
 
-    public bool IsAutoTargetingEnabled()
-    {
-        if (!IsEnabled)
-        {
-            PluginLog.Debug($"[ConflictingPlugins] [{PluginName}] is not enabled.");
-            return false;
-        }
-        
-        PluginLog.Verbose(
-            $"[ConflictingPlugins] [{PluginName}] " +
-            $"Has `AutoTarget`: {AutoTarget}");
-
-        return AutoTarget;
-    }
-
     private bool IsMetaActionRetargeted
         (uint metaAction, string actionType, out string stackName)
     {
